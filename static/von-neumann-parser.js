1320fdf2dc4cebe7755d124e038c042b1953fbfb
My main account <sf2164229838@sina.com>
2016-09-24 23:03:23 +0800

    Update von-neumann-parser.js

--- static/von-neumann-parser.js
+++ static/von-neumann-parser.js
@@ -3,7 +3,7 @@ var parsePart_vN = function (s) {
   var i = 0, value = 0;
   (s[i] === "0") && (++i, (value |= 1));
   (s[i] === "1") && (++i, (value |= 278));
-  if (s[i] === 2) {
+  if (s[i] === "2") {
 	++i;
 	(s[i] === "r") ? (++i, (value |= 4680)) : (s[i] === "i") ? (++i, (value |= 1056)) : (value |= 5736);
   }
