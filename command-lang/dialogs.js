e67b106580d3843ffbeb2df89352e642fa0392a7
My main account <sf2164229838@sina.com>
2016-10-19 13:59:31 +0800

    Update dialogs.js

--- command-lang/dialogs.js
+++ command-lang/dialogs.js
@@ -12,7 +12,7 @@ function _cprompt (title, value) {
 	var e = document.createElement("div");
 	value == null && (value = "");
 	e.className = "o0";
-	e.innerHTML = "<div class=\"o1\"><\/div><div class=\"o2\"><div class=\"o3\">" + title + "<\/div><div class=\"o6\">请输入字符串:<br><input type=\"text\" class=\"o7\" value=\"" + value + "\" /><\/div><div class=\"o4\" onclick=\"_pCan(this)\">取消<\/div><div class=\"o5\" onclick=\"_pOK(this)\">确认<\/div><\/div>"
+	e.innerHTML = "<div class=\"o1\"><\/div><div class=\"o2\"><div class=\"o3\">" + HTMLEscape(title) + "<\/div><div class=\"o6\">请输入字符串:<br /><input type=\"text\" class=\"o7\" value=\"" + HTMLEscape(value) + "\" /><\/div><div class=\"o4\" onclick=\"_pCan(this)\">取消<\/div><div class=\"o5\" onclick=\"_pOK(this)\">确认<\/div><\/div>"
 	document.getElementById("_dialogs").appendChild(e);
 }
 // ]]>
