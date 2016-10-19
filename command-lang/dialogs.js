820ef4eb680d3457f09c723d87b9ba220801dd46
My main account <sf2164229838@sina.com>
2016-10-19 15:52:12 +0800

    Update dialogs.js

--- command-lang/dialogs.js
+++ command-lang/dialogs.js
@@ -17,4 +17,22 @@ function _cprompt (title, value) {
 	e.innerHTML = "<div class=\"o1\"><\/div><div class=\"o2\"><div class=\"o3\">" + HTMLEscape(title) + "<\/div><div class=\"o6\">请输入字符串:<br /><input type=\"text\" class=\"o7\" value=\"" + HTMLEscape(value) + "\" /><\/div><div class=\"o4\" onclick=\"_pCan(this)\">取消<\/div><div class=\"o5\" onclick=\"_pOK(this)\">确认<\/div><\/div>"
 	document.getElementById("_dialogs").appendChild(e);
 }
+function continueF (n) {
+	if (n === 1) return CommandStepOverC();
+	if (n === 2) return CommandRun();
+}
+function CommandStepOverC () {
+	if (!iscompiled || !stillRunning) return;
+	// prevsp = callsp; stepO1 = 1;
+	if (!breakpoint && callsp > prevsp) return;
+	do {
+		KernelStep(compiled[lineNum]);
+	} while (!breakpoint && callsp > prevsp);
+	breakpoint = false;
+	$id("COutput").innerHTML = hout;
+	$id("CDebug").innerHTML = "Line " + (lineNum + 1) + ": " + compiled[lineNum];
+	if (rframe && callsp > prevsp) {
+		requestAnimationFrame(CommandRun);
+	}
+};
 // ]]>
