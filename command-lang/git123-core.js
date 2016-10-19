99e6ef1fb3abc30538ccc9280afdec03bac0b7a5
My main account <sf2164229838@sina.com>
2016-10-19 17:32:14 +0800

    Update git123-core.js

--- command-lang/git123-core.js
+++ command-lang/git123-core.js
@@ -49,7 +49,7 @@ function parseFmt1 (str) { // 格式变量
 			case "~tab": // 制表符
 				return "\t";
 			// break;
-			case "~tline": // 总行数
+			case "~cline": // 当前行号
 				return +lineNum + +$1p2;
 			// break
 			case "~tline": // 总行数
