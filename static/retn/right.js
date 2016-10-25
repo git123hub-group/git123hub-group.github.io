2006aeecf3089dadbf0e4d40305d16766c572f54
My main account <sf2164229838@sina.com>
2016-10-25 02:59:34 -0500

    Add files via upload

---
+++ static/retn/right.js
@@ -0,0 +1,14 @@
+var doc_head = document.head || document.getElementsByTagName("head")[0];
+var doc_body = document.body || document.getElementsByTagName("body")[0];
+
+var cssP1 = document.createElement("link");
+	cssP1.rel = "stylesheet";
+	cssP1.href = "https://git123hub.github.io/static/retn/right.css";
+	doc_head.appendChild(cssP1);
+
+var cssP1b = document.createElement("div");
+	cssP1b.className = "linkh"
+	cssP1b.onclick = function (event) {
+		location.href = "/index/"
+	}
+	doc_body.appendChild(cssP1b);
