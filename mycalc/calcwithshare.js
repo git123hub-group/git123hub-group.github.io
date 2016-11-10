95bfbd2aa46f8bbc97560692c2459ba6009506df
My main account <sf2164229838@sina.com>
2016-11-10 21:31:01 +0800

    Update calcwithshare.js

--- mycalc/calcwithshare.js
+++ mycalc/calcwithshare.js
@@ -1,11 +1,13 @@
 // <![CDATA[
 	var memory = 0, memstk = [];
 	function pushmem () {
-		memstk.push(ival);
+		memstk.push(memory);
+		memory = ival
 	}
 	function popmem () {
 		inputting = "0"
-		document.getElementById("display1").value = ival = memstk.length !== 0 ? memstk.pop() : ival = 0;
+		document.getElementById("display1").value = ival = memory
+		memory = memstk.length === 0 ? 0 : memstk.pop;
 	}
 	var inputting = "0", ival = 0, havingDP = false;
 	var OP_ADD = 0, OP_SUB = 1, OP_MUL = 2, OP_DIV = 3;
