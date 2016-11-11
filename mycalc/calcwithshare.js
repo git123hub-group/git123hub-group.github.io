871bab2161afe7d5c6cba40f871612e3770187e2
My main account <sf2164229838@sina.com>
2016-11-11 08:08:20 +0800

    Add files via upload

--- mycalc/calcwithshare.js
+++ mycalc/calcwithshare.js
@@ -1,9 +1,12 @@
 // <![CDATA[
-	var accAdd, accSub, accMul, accDiv;
+	var BigNumber;
 	var acccalc = document.createElement("script");
-	acccalc.src = "acccalc.js";
+	acccalc.src = "bignumber.min.js";
 	document.body.appendChild(acccalc);
-	var memory = 0, memstk = [];
+	acccalc.onload = function () {
+		memory = ival = new BigNumber(0)
+	}
+	var memory, memstk = [];
 	function pushmem () {
 		memstk.push(memory);
 		memory = ival
@@ -13,13 +16,13 @@
 		document.getElementById("display1").value = ival = memory
 		memory = memstk.length === 0 ? 0 : memstk.pop();
 	}
-	var inputting = "0", ival = 0, havingDP = false;
+	var inputting = "0", ival, havingDP = false;
 	var OP_ADD = 0, OP_SUB = 1, OP_MUL = 2, OP_DIV = 3;
 	var oplevel = [0,0,1,1,-1], nestParens = 0;
 	function negateb () {
 		var isneg = inputting.charAt(0) === "-";
 		inputting = isneg ? inputting.slice(1) : "-" + inputting;
-		ival = +inputting;
+		ival = new BigNumber(inputting);
 		document.getElementById("display1").value = inputting;
 	}
 	function evopr () {
@@ -32,16 +35,16 @@
 				var last1 = numstk.length - 1;
 				switch (lastOp) {
 					case OP_ADD:
-						numstk[last1] = accAdd(numstk[last1], tmp);
+						numstk[last1] = numstk[last1].add(tmp);
 					break;
 					case OP_SUB:
-						numstk[last1] = accSub(numstk[last1], tmp);
+						numstk[last1] = numstk[last1].sub(tmp);
 					break;
 					case OP_MUL:
-						numstk[last1] = accMul(numstk[last1], tmp);
+						numstk[last1] = numstk[last1].mul(tmp);
 					break;
 					case OP_DIV:
-						numstk[last1] = accDiv(numstk[last1], tmp);
+						numstk[last1] = numstk[last1].div(tmp);
 					break;
 				}
 				// OPR2 = lastOp;
@@ -68,16 +71,16 @@
 			var last1 = numstk.length - 1;
 			switch (lastOp) {
 				case OP_ADD:
-					numstk[last1] = accAdd(numstk[last1], tmp);
+					numstk[last1] = numstk[last1].add(tmp);
 				break;
 				case OP_SUB:
-					numstk[last1] = accSub(numstk[last1], tmp);
+					numstk[last1] = numstk[last1].sub(tmp);
 				break;
 				case OP_MUL:
-					numstk[last1] = accMul(numstk[last1], tmp);
+					numstk[last1] = numstk[last1].mul(tmp);
 				break;
 				case OP_DIV:
-					numstk[last1] = accDiv(numstk[last1], tmp);
+					numstk[last1] = numstk[last1].div(tmp);
 				break;
 			}
 			// OPR2 = lastOp;
@@ -94,14 +97,14 @@
 	function entern (str) {
 		evopr();
 		inputting = (inputting === "0" ? str : inputting + str);
-		ival = +inputting;
+		ival = new BigNumber(inputting);
 		document.getElementById("display1").value = inputting;
 	}
 	function enterdp () {
 		evopr();
 		havingDP || (inputting += ".");
 		havingDP = true;
-		ival = +inputting;
+		ival = new BigNumber(inputting);
 		document.getElementById("display1").value = inputting;
 	}
 	function backsp () {
@@ -109,7 +112,7 @@
 		inputting.slice(-1) === "." && (havingDP = false);
 		inputting = inputting.slice(0,-1);
 		inputting === "" && (inputting = "0");
-		ival = +inputting;
+		ival = new BigNumber(inputting);
 		document.getElementById("display1").value = inputting;
 	}
 	function mrecall () {
@@ -127,15 +130,15 @@
 		havingDP = false;
 	}
 	function sqrtkey () {
-		ival = Math.sqrt(ival);
+		ival = ival.sqrt();
 		document.getElementById("display1").value = ival;
 	}
 	function allclear () {
-		numstk = [], oprsstk = [], havingDP = isoprsmode = false, nestParens = ival = 0,
+		numstk = [], oprsstk = [], havingDP = isoprsmode = false, nestParens = ival = new BigNumber(0),
 		document.getElementById("display1").value = inputting = "0";
 	}
 	function clear1 () {
-		ival = 0, document.getElementById("display1").value = inputting = "0"; havingDP = false;
+		ival = new BigNumber(0), document.getElementById("display1").value = inputting = "0"; havingDP = false;
 	}
 	window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"32"},"slide":{"type":"slide","bdImg":"0","bdPos":"right","bdTop":"100"},"image":{"viewList":["qzone","tsina","tqq","renren","weixin"],"viewText":"分享到：","viewSize":"32"},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='https://git123hub.github.io/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
 // ]]>
